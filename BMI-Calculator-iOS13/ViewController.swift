10c2e0c8f73bf689c6715af445cccfec177caa32
Abdul R. Arraisi <itsarraisi@gmail.com>
2020-08-23 12:52:54 +0700

    Initial Commit

---
+++ BMI-Calculator-iOS13/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BMI-Calculator-iOS13
+//
+//  Created by Abdul R. Arraisi on 23/08/20.
+//  Copyright © 2020 Abdul R. Arraisi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
